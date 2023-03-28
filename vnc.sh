9b780f229f0c698c54489e541e71182652409a7f
a9281639a <128734532+a9281639a@users.noreply.github.com>
2023-03-28 12:30:31 -0400

    Create vnc.sh

---
+++ vnc.sh
@@ -0,0 +1,8 @@
+sudo su
+
+sudo apt-get install screen -y
+
+
+#Activating screen
+#pushing docker ubuntu desktop using screen (you can change resolution from below code) 
+screen -d -m docker run -p 8080:80 -e RESOLUTION=1024x1080 -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc
